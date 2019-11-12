32248c02357c390815cc5870993ae988bc3a33b4
Ngoc Dao <ngocdaothanh@gmail.com>
2019-11-12 18:10:35 +1100

    Write output to files

--- src/file.js
+++ src/file.js
@@ -1,6 +1,26 @@
+const cheerio = require('cheerio')
+const fs = require('fs')
+
 const getCppHeaderBaseFileName = (cppHeaderFileName) =>
   cppHeaderFileName.substring(0, cppHeaderFileName.indexOf('.'))
 
+const loadCastXml = (fileName) => {
+  // https://github.com/Leonidas-from-XIV/node-xml2js
+  const xml = fs.readFileSync(fileName)
+  return cheerio.load(xml, {xmlMode: true})
+}
+
+const writeOutput = (files) => {
+  const fileNames = Object.keys(files)
+  for (const fileName of fileNames) {
+    const content = files[fileName]
+    console.log(`${fileName} ~${content.length} bytes`)
+    fs.writeFileSync(`output/${fileName}`, content)
+  }
+}
+
 module.exports = {
-  getCppHeaderBaseFileName
+  getCppHeaderBaseFileName,
+  loadCastXml,
+  writeOutput
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
-const fs = require('fs')
-const cheerio = require('cheerio')
-
 const {collectDeclarations} = require('./declarations')
+const {loadCastXml, writeOutput} = require('./file')
 const {renderC} = require('./render-c')
 const {renderSwift} = require('./render-swift')
 
-// const CAST_XML = 'input/SkSize.xml'
-// const CPP_HEADER = 'SkSize.h'
+const CAST_XML = 'input/SkSize.xml'
+const CPP_HEADER = 'SkSize.h'
 
-const CAST_XML = 'input/SkCanvas.xml'
-const CPP_HEADER = 'SkCanvas.h'
-
-const loadXml = (fileName) => {
-  // https://github.com/Leonidas-from-XIV/node-xml2js
-  const xml = fs.readFileSync(fileName)
-  return cheerio.load(xml, {xmlMode: true})
-}
-
-const writeOutput = (files) => {
-  const fileNames = Object.keys(files)
-  for (const fileName of fileNames) {
-    const content = files[fileName]
-    console.log(fileName)
-    console.log(content)
-  }
-}
+// const CAST_XML = 'input/SkCanvas.xml'
+// const CPP_HEADER = 'SkCanvas.h'
 
 async function main() {
-  const $ = await loadXml(CAST_XML)
+  const $ = await loadCastXml(CAST_XML)
   const declarations = collectDeclarations($, CPP_HEADER)
 
   writeOutput(renderC($, declarations, CPP_HEADER))
