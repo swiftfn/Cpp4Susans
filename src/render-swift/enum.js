6b1f55f938df9ed5185d75e46949f914e97b9da0
Ngoc Dao <ngocdaothanh@gmail.com>
2019-11-14 11:59:22 +1100

    Swift: Use static for duplicate enum values

--- src/render-swift/enum.js
+++ src/render-swift/enum.js
@@ -1,14 +1,25 @@
-const renderValue = ($, value) => {
-  const node = $(value)
-  const name = node.attr('name')
-  const init = node.attr('init')
-  return `${name} = ${init}`
-}
+const {renderParts} = require('../render-util/groups')
 
-const renderValues = ($, values) =>
-  values.map((idx, value) =>
-    '  ' + renderValue($, value)
-  ).get().join(',\n')
+const renderValues = ($, values) => {
+  const inits = []
+  const uniques = []
+  const duplicates = []
+
+  values.each((idx, value) => {
+    const node = $(value)
+    const name = node.attr('name')
+    const init = node.attr('init')
+
+    if (inits.includes(init)) {
+      duplicates.push(`  public static let ${name} = ${init}`)
+    } else {
+      uniques.push(`  ${name} = ${init}`)
+      inits.push(init)
+    }
+  })
+
+  return renderParts([uniques.join(',\n'), duplicates.join('\n')])
+}
 
 const renderEnum = ($, declaration) => {
   const {node, values} = declaration
